6a92605512dd9035ed23c10ee49dea3217449fb0
scruz <jsebas.ct@gmail.com>
2022-12-03 11:13:39 -0400

    aoc day 1 - my solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val caloriesElf = mutableListOf<Int>()
+
+        var sum = 0
+        for (calorie in input) {
+            if (calorie != "") {
+                sum += calorie.toInt()
+            } else {
+                caloriesElf.add(sum)
+                sum = 0
+            }
+        }
+
+        return caloriesElf.maxOrNull() ?: 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val caloriesElf = mutableListOf<Int>()
+
+        var sum = 0
+        for (calorie in input) {
+            if (calorie != "") {
+                sum += calorie.toInt()
+            } else {
+                caloriesElf.add(sum)
+                sum = 0
+            }
+        }
+        caloriesElf.add(sum)
+        caloriesElf.sortDescending()
+
+        return caloriesElf.take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01_test")
     println(part1(input))
+    println("Part two")
     println(part2(input))
+
+    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day01_test")
+//    check(part2(testInput) == 45000)
 }
